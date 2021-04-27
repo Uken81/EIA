02b1f4bbba8f9057e67c02e02d45fe164efe2c17
Uken81 <64936770+Uken81@users.noreply.github.com>
2021-04-27 10:00:27 +1000

    refactor outputscript.js to handle inputs automatically instead of individually

--- outputscript.js
+++ outputscript.js
@@ -1,75 +1,80 @@
-    //Name
-    const placeFname = document.getElementById("customer-name"); 
-    let fName = localStorage.getItem("fname");
-    let lName = localStorage.getItem("lname");
-    console.log(lName);
-    placeFname.innerHTML = `${fName} ${lName}`;
-    //Address
-    const placeAddress = document.getElementById("customer-address");
-    let address = localStorage.getItem("address");
-    placeAddress.innerHTML = address;
-    //Phone
-    const placehPhone = document.getElementById("customer-phone-home");
-    const placeMPhone = document.getElementById("customer-phone-mobile");
-    let hphone = localStorage.getItem("hphone");
-    let mphone = localStorage.getItem("mphone");
-    placehPhone.innerHTML = hphone;
-    placeMPhone.innerHTML = mphone;
+let listArray;
+//converts the key list from script.js back to an array.
+const backToArray = () => {
+    const listString = localStorage.getItem("list");
+    listArray = listString.split(',');
+    console.log(listString);
+    console.log(listArray);
+}
 
-    const output = document.getElementById("output-page1");
-    function getPDF() {
-        output.style.opacity = 10;
-        function getWidth() {
-            return Math.max(
-                document.body.scrollWidth,
-                document.documentElement.scrollWidth,
-                document.body.offsetWidth,
-                document.documentElement.offsetWidth,
-                document.documentElement.clientWidth
-            );
+const renderValue = () => {
+    backToArray();
+    listArray.forEach(function (entry) {
+        let placeHereReference = `customer-${entry}`;
+        let placeHere = document.getElementById(placeHereReference);
+        let keyValue = localStorage.getItem(entry);
+        placeHere.innerHTML = keyValue;
+        console.log(placeHere);
+        console.log(keyValue);
+    })
+}
+renderValue();
+
+function getPDF() {
+    const renderLocation = document.getElementById("output-page1");
+    renderLocation.style.opacity = 10;
+
+    function getWidth() {
+        return Math.max(
+            document.body.scrollWidth,
+            document.documentElement.scrollWidth,
+            document.body.offsetWidth,
+            document.documentElement.offsetWidth,
+            document.documentElement.clientWidth
+        );
+    }
+
+    function getHeight() {
+        return Math.max(
+            document.body.scrollHeight,
+            document.documentElement.scrollHeight,
+            document.body.offsetHeight,
+            document.documentElement.offsetHeight,
+            document.documentElement.clientHeight
+        );
+    }
+
+    const HTML_Width = getWidth();
+    const HTML_Height = getHeight();
+    console.log(HTML_Width);
+    console.log(HTML_Height);
+
+    const top_left_margin = 15;
+    const PDF_Width = HTML_Width + (top_left_margin * 2);
+    const PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+    const canvas_image_width = HTML_Width;
+    const canvas_image_height = HTML_Height;
+
+    const totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+
+
+    html2canvas(document.getElementById("output-page1")).then(function (canvas) {
+        canvas.getContext('2d');
+
+        console.log(canvas.height + "  " + canvas.width);
+
+
+        const imgData = canvas.toDataURL("image/jpeg", 1.0);
+        const pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+        pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+
+
+        for (var i = 1; i <= totalPDFPages; i++) {
+            pdf.addPage(PDF_Width, PDF_Height);
+            pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
         }
-    
-        function getHeight() {
-            return Math.max(
-                document.body.scrollHeight,
-                document.documentElement.scrollHeight,
-                document.body.offsetHeight,
-                document.documentElement.offsetHeight,
-                document.documentElement.clientHeight
-            );
-        }
-        
-        const HTML_Width = getWidth();
-        const HTML_Height = getHeight();
-        console.log(HTML_Width);
-        console.log(HTML_Height);
-    
-        const top_left_margin = 15;
-        const PDF_Width = HTML_Width + (top_left_margin * 2);
-        const PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
-        const canvas_image_width = HTML_Width;
-        const canvas_image_height = HTML_Height;
-    
-        const totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
-    
-    
-        html2canvas(document.getElementById("output-page1")).then(function (canvas) {
-            canvas.getContext('2d');
-    
-            console.log(canvas.height + "  " + canvas.width);
-    
-    
-            const imgData = canvas.toDataURL("image/jpeg", 1.0);
-            const pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
-            pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
-    
-    
-            for (var i = 1; i <= totalPDFPages; i++) {
-                pdf.addPage(PDF_Width, PDF_Height);
-                pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
-            }
-    
-            pdf.save(`audit/${address}.pdf`);
-        });
-        output.style.opacity = 0;
-    };
+        const address = localStorage.getItem("address");
+        pdf.save(`audit_${address}.pdf`);
+    });
+    renderLocation.style.opacity = 0;
+};
--- script.js
+++ script.js
@@ -3,25 +3,33 @@ const createNameArray = () => {
     let inputArray = Array.from(document.getElementsByClassName('input-field'));
     inputArray.forEach(function(entry) {
         inputList.push(entry.name);
+        console.log(inputList);
     })
-}
-
-const createKeyPair = key => {
-   const jobInfo = document.getElementById(`${key}`);
-   const value = jobInfo.value;
-   localStorage[key] = value;    
 };
 
-const submitValues = () => {
+const createKeyValuePair = key => {
+   const formEntryKey = document.getElementById(`${key}`);
+   console.log(formEntryKey);
+   const formEntryValue = formEntryKey.value;
+   console.log(formEntryValue);
+   localStorage[key] = formEntryValue;    
+};
+
+const submitformEntryValues = () => {
     createNameArray();
     inputList.forEach(function (entry) {
-        createKeyPair(entry);
+        createKeyValuePair(entry);
     })   
-}
+};
+//sets input list in local storage so outputList.js can access.
+const submitList = () => {
+    localStorage.setItem("list", inputList);
+};
 
 document.getElementById("name-submit").addEventListener("click", function(event) {
     event.preventDefault();
-    submitValues();
+    submitformEntryValues();  
+    submitList();
     
 })
 
